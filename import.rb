d3f714e65ceda3b0617e9c56bd0d92fa3c2709f7
Tim Gentry <timgentry@users.noreply.github.com>
2021-04-28 11:58:06 +0100

    Corrected import file

--- import.rb
+++ import.rb
@@ -11,4 +11,4 @@ event_json = JSON.generate(event)
 
 puts event_json
 puts
-puts lambda_handler(event: JSON.parse(event_json), context: nil)
+puts Handler.process(event: JSON.parse(event_json), context: nil)
